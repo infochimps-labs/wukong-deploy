b9ad625750c39f76086b7692f4e41f36e461ee18
Dhruv Bansal <dhruv@ph.utexas.edu>
2013-06-04 03:00:56 -0500

    fixed doc task to include Wukong's doc helpers

--- lib/wukong-deploy/tasks.rb
+++ lib/wukong-deploy/tasks.rb
@@ -1,7 +1,12 @@
 require 'rake'
 
+require 'wukong/rake_helper'
+
 require 'yard'
-YARD::Rake::YardocTask.new(:doc)
+YARD::Rake::YardocTask.new(:doc) do |t|
+  t.files   = %w[app/**/*.rb Rakefile Gemfile]
+  t.options = ['--load', Wukong.doc_helpers_path]
+end
 
 require 'rspec/core/rake_task'
 RSpec::Core::RakeTask.new(:spec)
